0502b711022b6ab77cde967ed29365f9877b27d6
MohammadYasserZaki <mohammad.yasser@ontariotechu.net>
2025-05-20 20:21:10 -0300

    Initial commit with CMake, Catch2, and test setup

---
+++ tests/test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mohammad on 2025-05-20.
+//
+
+#include "test.h"
---
+++ tests/test.h
@@ -0,0 +1,16 @@
+//
+// Created by Mohammad on 2025-05-20.
+//
+
+#ifndef TEST_H
+#define TEST_H
+
+
+
+class test {
+
+};
+
+
+
+#endif //TEST_H
